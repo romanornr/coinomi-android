d6a3d100abb9c9a8629ab9363eb27d8ea2046bf1
romanornr <romanornr@gmail.com>
2016-12-25 14:32:24 +0100

    Add Viacoin support Mainnet

---
+++ core/src/main/java/com/coinomi/core/coins/ViacoinMain.java
@@ -0,0 +1,31 @@
+package com.coinomi.core.coins;
+
+import com.coinomi.core.coins.families.BitFamily;
+
+public Class ViacoinMain extends BitFamily {
+    private ViacoinMain() {
+        id = "viacoin.main";
+
+        addressHeader = 71;
+        p2shHeader = 33;
+        acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
+        spendableCoinbaseDepth = 100;
+        dumpedPrivateKeyHeader = 199;
+
+        name = "Viacoin";
+        symbol = "VIA";
+        uriScheme  = "viacoin";
+        bip44Index = 14;
+        unitExponent = 8;
+        feeValue = value(100000);
+        minNonDust = value(1000); // VIA miniput
+        softDusLimit = value(100000); // 0.001 VIA
+        softDustPolicy = SoftDustPolicy.BASE_FEE_FOR_EACH_SOFT_DUST_TXO;
+        signedMessageHeader = toBytes("Viacoin Signed Message:\n");
+    }
+
+    private static ViacoinMain instance = new ViacoinMain();
+    public static synchronized CoinType get() {
+        return instance;
+    }
+}
--- wallet/src/main/java/com/coinomi/wallet/Constants.java
+++ wallet/src/main/java/com/coinomi/wallet/Constants.java
@@ -47,6 +47,7 @@ import com.coinomi.core.coins.VertcoinMain;
 import com.coinomi.core.coins.VpncoinMain;
 import com.coinomi.core.coins.RichcoinMain;
 import com.coinomi.core.coins.IxcoinMain;
+import com.coinomi.core.coins.ViacoinMain;
 import com.coinomi.core.network.CoinAddress;
 import com.coinomi.stratumj.ServerAddress;
 import com.google.common.collect.ImmutableList;
@@ -214,8 +215,10 @@ public class Constants {
             new CoinAddress(RichcoinMain.get(),     new ServerAddress("richx-cce-1.coinomi.net", 5046),
                                                     new ServerAddress("richx-cce-2.coinomi.net", 5046)),
             new CoinAddress(IxcoinMain.get(),       new ServerAddress("ixc-cce-1.coinomi.net", 5047),
-                                                    new ServerAddress("ixc-cce-2.coinomi.net", 5047))
-    );
+                                                    new ServerAddress("ixc-cce-2.coinomi.net", 5047)),
+            new CoinAddress(ViacoinMain.get(),      new ServerAddress("server.vialectrum.org", 50001),
+                                                    new ServerAddress("vialectrum.viacoin.net", 50001),
+                                                    new ServerAddress("vialectrum.bitops.me", 50001));
 
     public static final HashMap<CoinType, Integer> COINS_ICONS;
     public static final HashMap<CoinType, String> COINS_BLOCK_EXPLORERS;
@@ -264,6 +267,7 @@ public class Constants {
         COINS_ICONS.put(CoinID.CLUBCOIN_MAIN.getCoinType(), R.drawable.clubcoin);
         COINS_ICONS.put(CoinID.RICHCOIN_MAIN.getCoinType(), R.drawable.richcoin);
         COINS_ICONS.put(CoinID.IXCOIN_MAIN.getCoinType(), R.drawable.ixcoin);
+        COINS_ICONS.put(CoinID.VIACOIN_MAIN.getCoinType(), R.drawable.viacoin);
 
         COINS_BLOCK_EXPLORERS = new HashMap<CoinType, String>();
         COINS_BLOCK_EXPLORERS.put(CoinID.BITCOIN_MAIN.getCoinType(), "https://blockchain.info/tx/%s");
@@ -306,6 +310,7 @@ public class Constants {
         COINS_BLOCK_EXPLORERS.put(CoinID.EGULDEN_MAIN.getCoinType(), "https://chainz.cryptoid.info/efl/tx.dws?%s");
         COINS_BLOCK_EXPLORERS.put(CoinID.RICHCOIN_MAIN.getCoinType(), "https://explorer.richcoin.us/transaction?transaction=%s");
         COINS_BLOCK_EXPLORERS.put(CoinID.IXCOIN_MAIN.getCoinType(), "https://chainz.cryptoid.info/ixc/tx.dws?%s");
+        COINS_BLOCK_EXPLORERS.put(CoinID.VIACOIN_MAIN.getCoinType(), "https://chainz.cryptoid.info/via/tx.dws?%s"
     }
 
     public static final CoinType DEFAULT_COIN = BitcoinMain.get();
@@ -358,6 +363,7 @@ public class Constants {
             VpncoinMain.get(),
             BitcoinTest.get(),
             LitecoinTest.get(),
-            DogecoinTest.get()
+            DogecoinTest.get(),
+            ViacoinMain.get()
     );
 }
